9e1cda2c5978ccd4da4d3a53398a4096c51bd220
0xhatsume <0xhatsume@gmail.com>
2021-12-11 23:04:04 +0800

    build: initial hardhat boiler

---
+++ hardhat.config.ts
@@ -0,0 +1,59 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "hardhat-watcher";
+import "solidity-coverage";
+import "./tasks/deploy";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ]
+  },
+  networks: {
+    ftm: {
+      url: "https://rpc.ftm.tools/",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    ftmtestnet: {
+      url: "https://rpc.testnet.fantom.network/",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+  gasReporter: { enabled: true, currency: "USD"},
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
