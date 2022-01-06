518333318cd4694d79e217ffd1069fc43c61d0f9
0xhatsume <0xhatsume@gmail.com>
2022-01-06 17:26:11 +0800

    feat: add inits for rooms and grids and placeholder shuffle function

--- contracts/House.sol
+++ contracts/House.sol
@@ -9,19 +9,33 @@ contract House is Ownable {
     address public habitants;
 
     // struct for rooms with attributes for rewards
+    // can be nfts
     struct Room {
-        uint256 room;
+        uint room;
         uint256 yield;
     }
 
     // lets have 8x8 grids first
-    uint256 public gridLength;
-    uint256 public gridWidth; // % 8 to get offset
-    mapping(uint256 => uint256) public roomLocation; //mapping to tell where are the rooms on the grids
-    mapping(uint256 => uint256) private habitantsToRooms; //mapping to tell which room is particular inhabitant at
+    uint public gridLength;
+    uint public gridWidth; // % 8 to get offset
+    mapping(uint => uint) public roomToGrid; //mapping to tell where are the rooms on the grids
+    mapping(uint256 => uint) private habitantsToRooms; //mapping to tell which room is particular inhabitant at
+    Room [] public roomList;
 
-    constructor (address _habitants){
+    constructor (address _habitants, uint _gridLength, uint _gridWidth){
+        // todo: insert require for max grid dims
         habitants = _habitants;
+        gridLength = _gridLength;
+        gridWidth = _gridWidth;
+
+        for (uint i; i<gridLength*gridWidth; i++){
+            roomToGrid[i] = i;
+            roomList[i] = Room(i,0);
+        }
+    }
+
+    function shuffle() external view returns(Room[] memory){
+        return roomList;
     }
     
 
